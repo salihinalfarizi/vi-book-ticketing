f9d7bea8f766dc5db8e5036f0cacd9abced303d4
Salihin Muhammad <muhammadnsalihin23@gmail.com>
2024-03-23 05:10:03 +0800

    fix: create authentication and user repository

---
+++ lib/data/repositories/authentication.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:vi_book/domain/entities/result.dart';
+
+abstract interface class Authentication {
+  Future<Result<String>> register(
+    {
+      required String email,
+      required String password,
+    }
+  );
+  Future<Result<String>> login(
+    {
+      required String email,
+      required String password,
+    }
+  );
+  Future<Result<void>> logout();
+  String? getLoggedInUserId();
+}
